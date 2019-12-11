4756a18650d7719f2659773234dde16e4d7ab4d8
Leonardo Pereira <leopereirasilva86@gmail.com>
2019-12-11 17:45:02 -0300

    REST testado

---
+++ src/main/java/com/leop/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.leop.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar( ) {
+		return "REST esta funcionando";
+	}
+}
